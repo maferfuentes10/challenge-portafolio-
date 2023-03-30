5bdfb1bf65b4563829ed899aa939b77009afe929
maferfuentes10 <119360146+maferfuentes10@users.noreply.github.com>
2023-03-30 11:59:02 -0500

    Add files via upload

---
+++ DESAFIO_02/js/main.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+    //Cambio de header//
+    $(window).on('scroll', function(){
+        let posicion = $('#pri').offset();
+        if($(window).scrollTop() >= posicion.top){
+            $('header').removeClass('transparente');
+        } else{
+            $('header').addClass('transparente');
+        }
+    })
+    //Accionamiento del menu lateral//
+    $('#burger').click(function(){
+        $('#burger').toggleClass('abierto');
+        $('#menu').toggleClass('abierto');
+    })
+    //Smooth scroll//
+    $('a').on('click', function(){
+        if(this.hash !== ''){
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800)
+        }
+    })
+})
